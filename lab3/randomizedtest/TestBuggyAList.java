d72c7ad45fa6dbdb73b15d475f7f1f16c2a7b489
WatsonthePeanut <fantrans.01@gmail.com>
2025-07-16 16:39:16 +0800

    done with lab3

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -9,4 +9,49 @@ import static org.junit.Assert.*;
  */
 public class TestBuggyAList {
   // YOUR TESTS HERE
+    @Test
+    public void testThreeAddThreeRemove() {
+        AListNoResizing<Integer> correct = new AListNoResizing<>();
+        BuggyAList<Integer> broken = new BuggyAList<>();
+        for (int i = 4; i < 7; ++i) {
+            correct.addLast(i);
+            broken.addLast(i);
+        }
+        assertEquals(correct.size(), broken.size());
+        assertEquals(correct.removeLast(), broken.removeLast());
+        assertEquals(correct.removeLast(), broken.removeLast());
+        assertEquals(correct.removeLast(), broken.removeLast());
+    }
+
+    @Test
+    public void randomizedTest() {
+        AListNoResizing<Integer> correct = new AListNoResizing<>();
+        BuggyAList<Integer> broken = new BuggyAList<>();
+
+        int N = 50000;
+        for (int i = 0; i < N; i += 1) {
+            int operationNumber = StdRandom.uniform(0, 4);
+            if (operationNumber == 0) {
+                // addLast
+                int randVal = StdRandom.uniform(0, 100);
+                correct.addLast(randVal);
+                broken.addLast(randVal);
+            } else if (operationNumber == 1) {
+                // size
+                assertEquals(correct.size(), broken.size());
+            } else if (operationNumber == 2) {
+                // getLast
+                if (correct.size() == 0) {
+                    continue;
+                }
+                assertEquals(correct.getLast(), broken.getLast());
+            } else if (operationNumber == 3) {
+                // removeLast
+                if (correct.size() == 0) {
+                    continue;
+                }
+                assertEquals(correct.removeLast(), broken.removeLast());
+            }
+        }
+    }
 }
--- lab3/timingtest/TimeSLList.java
+++ lab3/timingtest/TimeSLList.java
@@ -23,6 +23,24 @@ public class TimeSLList {
 
     public static void timeGetLast() {
         // TODO: YOUR CODE HERE
+        AList<Integer> Ns = new AList<>();
+        AList<Double> times = new AList<>();
+        AList<Integer> opCounts = new AList<>();
+        for (int i = 1000; i <= 128000; i *= 2) {
+            SLList<Integer> test = new SLList<>();
+            for (int j = 0; j < i; j++) {
+                test.addLast(j);
+            }
+            Stopwatch sw = new Stopwatch();
+            for (int m = 0; m < 10000; m++) {
+                test.getLast();
+            }
+            double timeInSeconds = sw.elapsedTime();
+            Ns.addLast(i);
+            times.addLast(timeInSeconds);
+            opCounts.addLast(10000);
+        }
+        printTimingTable(Ns, times, opCounts);
     }
 
 }
